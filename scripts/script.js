62b29b1391c9ff9a21c49aff8cda731c0a1aae9d
Rio Zakirahman <rio.zakirahman@binus.ac.id>
2023-01-27 11:05:39 +0700

    feat: add popup to add card

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,14 @@
 let profile = document.querySelector(".profile");
 let popUp = document.querySelector(".popup");
 let popUpForm = popUp.querySelector(".popup__container");
-let popUpClose = popUp.querySelector(".popup__close");
+
+let popUpClose = document.querySelectorAll(".popup .popup__close");
 let editBtn = profile.querySelector(".profile__edit-button");
 let cards = document.querySelector(".cards");
 let cardLike = cards.querySelectorAll(".card .card__like");
+let popUpAdd = document.querySelector(".popup_add");
+let popUpAddForm = popUpAdd.querySelector(".popup__container");
+let addBtn = profile.querySelector(".profile__add-button");
 
 //profile value
 let profileName = profile.querySelector(".profile__name");
@@ -24,11 +28,15 @@ function popupOpen() {
 }
 function popupClose() {
   popUp.classList.remove("popup_opened");
+  popUpAdd.classList.remove("popup_opened");
 }
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
+}
+function handleAddForm(evt) {
+  evt.preventDefault();
 }
 
 function activeLike(cardLike) {
@@ -37,17 +45,33 @@ function activeLike(cardLike) {
 function deactiveLike(cardLike) {
   cardLike.classList.remove("card__like_active");
 }
+function addCard() {
+  popUpAdd.classList.toggle("popup_opened");
+}
 
 editBtn.addEventListener("click", function () {
   popupOpen();
 });
-popUpClose.addEventListener("click", function () {
-  popupClose();
+popUpClose.forEach((e) => {
+  e.addEventListener("click", () => {
+    popupClose();
+  });
 });
+
 popUpForm.addEventListener("submit", function (evt) {
   handleProfileFormSubmit(evt);
   popupClose();
 });
+
+popUpAddForm.addEventListener("submit", (evt) => {
+  handleAddForm(evt);
+  popupClose();
+});
+
+addBtn.addEventListener("click", () => {
+  addCard();
+});
+
 cardLike.forEach((c) => {
   c.addEventListener("click", function () {
     c.classList.toggle("card__like_active");
